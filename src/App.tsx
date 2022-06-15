107b0d11d89ed4c183fc7d42747392566e1d3158
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-15 19:58:58 -0300

    Iniciando o Projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
-import Botao from './components/buttons';
+import Botao from './components/Buttons';
+import Formulario from './components/Forms'
+
 
 function App() {
   return (
     <div className="App">
       <Botao/>
+      <Formulario/>
     </div>
   );
 }
--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -1,11 +1,12 @@
-import React from 'React';
+import React from "react";
 
 class Botao extends React.Component {
-    
-    render() {
-        return (
-            <button>Clique Aqui</button>
+    render(){
+        return(
+            <button>
+                Clique-me!
+            </button>
         )
     }
 }
-export default Botao; 
+export default Botao;
