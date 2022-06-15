8598ebbffeb3c31c83a1e3c31ff880eb0143d76c
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-15 20:21:22 -0300

    Adicionando Componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Botao from './components/Buttons';
 import Formulario from './components/Forms'
-
+import Lista from './components/Lists'
 
 function App() {
   return (
     <div className="App">
-      <Botao/>
       <Formulario/>
+      <Botao/>
+      <Lista/>
     </div>
   );
 }
