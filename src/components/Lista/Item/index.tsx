3f4fda279144439dd94f4bbe66c8c6152326052f
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-16 15:44:38 -0300

    Selecionando uma

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,9 +1,19 @@
 import { ITarefa } from '../../../types/tarefa';
 import style from '../Lista.module.scss';
+
+interface Props extends ITarefa {
+    selecionaTarefa: (tarefaSelecionada: ITarefa) => void
+}
 //{props}:{tipo}
-export default function Item({ tarefa, tempo, selecionado, completado, id }: ITarefa) {
+export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
     return (
-        <li className={style.item}>
+        <li className={style.item}
+            onClick={() => selecionaTarefa(
+                {
+                    tarefa, tempo, selecionado, completado, id
+                }
+            )}
+        >
             <h3>{tarefa}</h3>
             <span>{tempo}</span>
         </li>
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -2,16 +2,22 @@ import { ITarefa } from '../../types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
+interface Props {
+  tarefas: ITarefa[],
+  selecionaTarefa: (tarefaSelecionada: ITarefa) => void 
 
+}
 
-function Lista({tarefas}: {tarefas: Array<ITarefa>}) {
+function Lista({tarefas, selecionaTarefa}: Props) {
 
   return (
     <aside className={style.listaTarefas}>
       <h2> Tasks do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <Item key={index}
+          <Item 
+          selecionaTarefa={selecionaTarefa}
+          key={index}
             {...item} //exibe tudo dentro de item
           />
         ))}
