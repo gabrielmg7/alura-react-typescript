022fd3084a81a86182af02ec75883ad5736ae7b9
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-16 16:11:59 -0300

    conversão de tempo

--- src/components/Lista/Item/index.tsx
+++ src/components/Lista/Item/index.tsx
@@ -1,5 +1,5 @@
 import { ITarefa } from '../../../types/tarefa';
-import style from '../Lista.module.scss';
+import style from './Item.module.scss';
 
 interface Props extends ITarefa {
     selecionaTarefa: (tarefaSelecionada: ITarefa) => void
@@ -7,7 +7,8 @@ interface Props extends ITarefa {
 //{props}:{tipo}
 export default function Item({ tarefa, tempo, selecionado, completado, id, selecionaTarefa }: Props) {
     return (
-        <li className={style.item}
+        <li className={`${style.item} 
+                        ${selecionado ? style.itemSelecionado : ''}`}
             onClick={() => selecionaTarefa(
                 {
                     tarefa, tempo, selecionado, completado, id
