b61a015747cfb3494325debdecbdb5ae809c956b
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-16 16:24:24 -0300

    atualizando cronômetro

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -1,11 +1,24 @@
 import Botao from "../Botao"
 import Relogio from "./Relogio"
 import style from "./Cronometro.module.scss"
+import { ITarefa } from "../../types/tarefa"
+import { useState } from "react"
+import { tempoParaSegundos } from "../../common/utils/time"
+interface Props {
+    selecionado: ITarefa | undefined
+}
 
-export default function Cronometro() {
+export default function Cronometro({ selecionado }: Props) {
+
+    const [tempo, setTempo] = useState<Number>();
+    if(selecionado?.tempo){
+        setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+
     return (
         <div className={style.cronometro}>
             <p className={style.titulo}>Escolha um e inicie o Cronômetro</p>
+            Tempo:{tempo}
             <div className={style.relogioWrapper}>
                 <Relogio />
             </div>
