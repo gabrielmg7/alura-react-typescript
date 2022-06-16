6a387ac960a3c4ebbbd5e1bf11949a2456d87d00
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-16 17:20:24 -0300

    finalizando

--- src/components/Cronometro/index.tsx
+++ src/components/Cronometro/index.tsx
@@ -13,17 +13,28 @@ export default function Cronometro({ selecionado }: Props) {
     const [tempo, setTempo] =
         useState<number>();
     useEffect(() => {
-        if(selecionado?.tempo){
-            setTempo(tempoParaSegundos(selecionado.tempo))}
+        if (selecionado?.tempo) {
+            setTempo(tempoParaSegundos(selecionado.tempo))
+        }
     }, [selecionado]);
+
+    function regressiva(contador: number = 0) {
+        setTimeout(() => {
+            if (contador > 0) {
+                setTempo(contador - 1);
+                return regressiva(contador - 1);
+            }
+        }, 1000)
+    }
 
     return (
         <div className={style.cronometro}>
-            <p className={style.titulo}>Escolha um e inicie o Cronômetro</p>
+            <p className={style.titulo}>Escolha a(s) task(s) e inicie o cronômetro</p>
             <div className={style.relogioWrapper}>
                 <Relogio tempo={tempo} />
             </div>
-            <Botao>Começar</Botao>
+            <Botao onClick={() => regressiva(tempo)}
+            >Começar</Botao>
         </div>
     )
 }
