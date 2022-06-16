83813e8d07355556b4517f3d99dd3200640e85ae
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-16 15:06:38 -0300

    atualizando a Tarefa

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -2,27 +2,15 @@ import React, { useState } from 'react';
 import Cronometro from '../components/Cronometro';
 import Formulario from '../components/Form';
 import Lista from '../components/Lista';
+import { ITarefa } from '../types/tarefa';
 import style from './App.module.scss';
 
 function App() {
-  const [tarefas, setTarefas] = useState(
-    [
-      {
-        tarefa: 'React',
-        tempo: '02:00:00'
-      }, {
-        tarefa: 'Javascript',
-        tempo: '01:00:00'
-      }, {
-        tarefa: "Typescript",
-        tempo: "03:00:00"
-      }
-    ]
-  );
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
   return (
     <div className={style.AppStyle}>
       <Formulario setTarefas={setTarefas} />
-      <Lista tarefas={tarefas}/>
+      <Lista tarefas={tarefas} />
       <Cronometro />
     </div>
   );
