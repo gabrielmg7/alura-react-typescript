e04167c18d253e9c15a2c79315093ff915fe8b3e
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-15 23:39:43 -0300

    Adicionando componentes e estilizando

---
+++ src/components/Lista/Item/index.tsx
@@ -0,0 +1,10 @@
+import style from '../Lista.module.scss';
+//{props}:{tipo}
+export default function Item({ tarefa, tempo }: { tarefa: string, tempo: string }) {
+    return (
+        <li className={style.item}>
+            <h3>{tarefa}</h3>
+            <span>{tempo}</span>
+        </li>
+    )
+}
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
@@ -14,13 +15,11 @@ function Lista() {
   }];
   return (
     <aside className={style.listaTarefas}>
-      <h2> Estudos do dia </h2>
+      <h2> Tasks do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className={style.item}>
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+         <Item key={index}
+          {...item}/> //exibe tudo dentro de item
         ))}
       </ul>
     </aside>
