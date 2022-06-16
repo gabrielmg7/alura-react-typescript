25d15d877a394a63e4593cb15ba0b75cb600e879
gabrielmg7 <gabrielmouraguimaraes@gmail.com>
2022-06-16 13:19:40 -0300

    entendendo states

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from './Item';
 import style from './Lista.module.scss';
 
 function Lista() {
-  const tarefas = [{
-    tarefa: 'React',
-    tempo: '02:00:00'
-  }, {
-    tarefa: 'Javascript',
-    tempo: '01:00:00'
-  }, {
-    tarefa: "Typescript",
-    tempo: "03:00:00"
-  }];
+  const [tarefas, setTarefas] = useState(
+    [
+      {
+        tarefa: 'React',
+        tempo: '02:00:00'
+      }, {
+        tarefa: 'Javascript',
+        tempo: '01:00:00'
+      }, {
+        tarefa: "Typescript",
+        tempo: "03:00:00"
+      }
+    ]
+  );
   return (
     <aside className={style.listaTarefas}>
-      <h2> Tasks do dia </h2>
+      <h2 onClick={() => {
+        setTarefas([...tarefas, {
+          tarefa: "Estudar state",
+          tempo: "05:00:00"
+        }])
+      }}> Tasks do dia </h2>
       <ul>
         {tarefas.map((item, index) => (
-         <Item key={index}
-          {...item}/> //exibe tudo dentro de item
+          <Item key={index}
+            {...item} //exibe tudo dentro de item
+          />
         ))}
       </ul>
     </aside>
